a32a69fa85454dc68e3f4bd32ba705bd2d4dcde2
zhongliheng <zhongliheng@shoyoo.com>
2022-06-10 11:24:58 +0800

    添加jsonMappingsDemo

---
+++ big_data/kafka/springKafka/jsonMappings/src/main/java/jsonMappingsDemo/Cat.java
@@ -0,0 +1,20 @@
+package jsonMappingsDemo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.beans.ConstructorProperties;
+
+@Setter
+@Getter
+public class Cat {
+    
+    private String name;
+    private int age;
+
+    @ConstructorProperties({"n","a"})
+    public Cat(String n, int a) {
+        this.name = n;
+        this.age = a;
+    }
+}
